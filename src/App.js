30891fa4bf01582a0a3d1531d844130079070a9a
badri119 <badrinathv98@gmail.com>
2023-11-15 22:50:24 -0800

    small changes

--- src/App.js
+++ src/App.js
@@ -6,27 +6,70 @@ import NavBar from "./components/NavBar";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import SocialLinks from "./components/SocialLinks";
-import {Helmet} from "react-helmet";
-import {IoChevronUp} from 'react-icons/io5'
+import { Helmet } from "react-helmet";
+import { IoChevronUp } from "react-icons/io5";
+import React, { useEffect, useRef, useState } from "react";
 
+const RevealOnScroll = ({ children }) => {
+  const [isVisible, setIsVisible] = useState(false);
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const scrollObserver = new IntersectionObserver(([entry]) => {
+      if (entry.isIntersecting) {
+        setIsVisible(true);
+        scrollObserver.unobserve(entry.target);
+      }
+    });
+
+    scrollObserver.observe(ref.current);
+
+    return () => {
+      if (ref.current) {
+        scrollObserver.unobserve(ref.current);
+      }
+    };
+  }, []);
+
+  const classes = `transition-opacity duration-1000   
+      ${isVisible ? "opacity-100" : "opacity-0"}`;
+  return (
+    <div ref={ref} className={classes}>
+      {children}
+    </div>
+  );
+};
 function App() {
   return (
-    <div id='top'>
-      <div className=" fixed w-10 h-10 rounded-full flex justify-center items-center bg-black z-10 bottom-5 right-10"><a href="#top"><IoChevronUp color="white" size={30}/></a></div>
-       <Helmet>
-                <meta charSet="utf-8" />
-                <title>Portfolio</title>
-                <meta name="description" content="Portfolio" />
-            </Helmet>
-      <NavBar/>
-      <Home/>
-      <About/>
-      <Projects/>
-      <Skills/> 
-      <Cycling/>
-      <Contact/>
-      <SocialLinks/>
-    
+    <div id="top">
+      <div className=" fixed w-10 h-10 rounded-full flex justify-center items-center bg-black z-10 bottom-5 right-10">
+        <a href="#top">
+          <IoChevronUp color="white" size={30} />
+        </a>
+      </div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Portfolio</title>
+        <meta name="description" content="Portfolio" />
+      </Helmet>
+      <NavBar />
+      <Home />
+      <SocialLinks />
+      <RevealOnScroll>
+        <About />
+      </RevealOnScroll>
+      <RevealOnScroll>
+        <Projects />
+      </RevealOnScroll>
+      <RevealOnScroll>
+        <Skills />
+      </RevealOnScroll>
+      <RevealOnScroll>
+        <Cycling />
+      </RevealOnScroll>
+      <RevealOnScroll>
+        <Contact />
+      </RevealOnScroll>
     </div>
   );
 }
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,82 +1,87 @@
-import React from 'react'
-import html from '../assets/html.png'
-import python from '../assets/python.png'
-import css from '../assets/css.png'
-import ionic from '../assets/ionic.png'
-import javascript from '../assets/javascript.png'
-import react from '../assets/react.png'
-import tailwind from '../assets/tailwind.png'
-import node from '../assets/node.png'
-import java from '../assets/java.png'
+import React from "react";
+import html from "../assets/html.png";
+import python from "../assets/python.png";
+import css from "../assets/css.png";
+import ionic from "../assets/ionic.png";
+import javascript from "../assets/javascript.png";
+import react from "../assets/react.png";
+import tailwind from "../assets/tailwind.png";
+import node from "../assets/node.png";
+import java from "../assets/java.png";
 
 const Skills = () => {
-    const techs = [
-        {
-          id: 1,
-          src: html,
-          title: "HTML",
-          style: "shadow-orange-500",
-        },
-        {
-          id: 2,
-          src: css,
-          title: "CSS",
-          style: "shadow-blue-500",
-        },
-        {
-          id: 3,
-          src: javascript,
-          title: "JavaScript",
-          style: "shadow-yellow-500",
-        },
-        {
-            id: 4,
-            src: python,
-            title: "Python",
-            style: "shadow-yellow-500",
-          },
-          {
-            id: 6,
-            src: java,
-            title: "Java",
-            style: "shadow-red-600",
-          },
-        {
-          id: 6,
-          src: react,
-          title: "React",
-          style: "shadow-blue-600",
-        },
-        {
-          id: 7,
-          src: tailwind,
-          title: "Tailwind",
-          style: "shadow-sky-400",
-        },
-        {
-          id: 8,
-          src: ionic,
-          title: "Ionic",
-          style: "shadow-blue-500",
-        },
-       
-        {
-            id: 9,
-            src: node,
-            title: "Node",
-            style: "shadow-green-500",
-          },
-        
-      ];
+  const techs = [
+    {
+      id: 1,
+      src: html,
+      title: "HTML",
+      style: "shadow-orange-500",
+    },
+    {
+      id: 2,
+      src: css,
+      title: "CSS",
+      style: "shadow-blue-500",
+    },
+    {
+      id: 3,
+      src: javascript,
+      title: "JavaScript",
+      style: "shadow-yellow-500",
+    },
+    {
+      id: 4,
+      src: python,
+      title: "Python",
+      style: "shadow-yellow-500",
+    },
+    {
+      id: 6,
+      src: java,
+      title: "Java",
+      style: "shadow-red-600",
+    },
+    {
+      id: 6,
+      src: react,
+      title: "React",
+      style: "shadow-blue-600",
+    },
+    {
+      id: 7,
+      src: tailwind,
+      title: "Tailwind",
+      style: "shadow-sky-400",
+    },
+    {
+      id: 8,
+      src: ionic,
+      title: "Ionic",
+      style: "shadow-blue-500",
+    },
+
+    {
+      id: 9,
+      src: node,
+      title: "Node",
+      style: "shadow-green-500",
+    },
+  ];
   return (
-    <div name="skills"
-    className="bg-gradient-to-b from-gray-800 to-black w-full h-screen">
-        <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
-            <div>
-                <p className="text-4xl font-bold inline border-b-4 border-gray-500">Skills</p>
-                <p className="py-6">These are the technologies that I have used so far:</p>
-            </div>
-            <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
+    <div
+      name="skills"
+      className="bg-gradient-to-b from-gray-800 to-black w-full "
+    >
+      <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
+        <div>
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            Skills
+          </p>
+          <p className="py-6">
+            These are the technologies that I have used so far:
+          </p>
+        </div>
+        <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
           {techs.map(({ id, src, title, style }) => (
             <div
               key={id}
@@ -87,9 +92,9 @@ const Skills = () => {
             </div>
           ))}
         </div>
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Skills
+export default Skills;
