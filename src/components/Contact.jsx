8b597dac5c8617f43b72f4e0940e600271927c1f
badri119 <badrinathv98@gmail.com>
2023-11-15 22:50:24 -0800

    small changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,23 +1,58 @@
-import React from 'react'
+import React from "react";
+import github from "../assets/github.jpeg";
+import linkedin from "../assets/linkedin.png";
 
 const Contact = () => {
   return (
-    <div name='contact' className="w-full h-screen bg-gradient-to-b from-gray-800 to-black  p-4 text-white"> 
-        <div className=' max-w-screen-lg p-30 mx-auto flex flex-col justify-center w-full h-full'> 
-            <div> 
-                <p className=' text-4xl font-bold inline border-b-4 border-gray-500 '>
-                  Contact 
-                </p>
-            </div>
-            <p className=' text-left text-xl mt-5'>
-            If you want to get in touch with me, please shoot an email to badrinathv98@gmail.com
-            </p>
-            <br/>
-        </div> 
+    <div
+      name="contact"
+      className="w-full bg-gradient-to-b from-gray-800 to-black  p-4 text-white"
+    >
+      <div className=" max-w-screen-lg p-30 mx-auto flex flex-col w-full h-full">
         <div>
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500 ">
+            Contact
+          </p>
         </div>
+        <div className=" flex flex-row gap-10 justify-center py-7">
+          {/* LinkedIn Box */}
+          <div className=" hover:scale-105 duration-500 shadow-lg shadow-blue-500 max-w-md rounded-lg overflow-hidden py-12 mb-6 w-full sm:w-96 text-center">
+            <div className=" flex justify-center content-center">
+              <img className="w-28 h-28 rounded-full" src={linkedin} />
+            </div>
+            <h2 className="text-xl font-semibold mb-2">Connect on LinkedIn</h2>
+            <div className="flex justify-center">
+              <a
+                href="https://www.linkedin.com/in/badrinath-vasudevan-3a3335163/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded inline-block hover:scale-105 duration-500"
+              >
+                Visit Profile
+              </a>
+            </div>
+          </div>
+          {/* GitHub*/}
+          <div className=" hover:scale-105 duration-500 shadow-lg shadow-white max-w-md rounded-lg overflow-hidden py-12 mb-6 w-full sm:w-96 text-center">
+            <div className=" flex justify-center content-center">
+              <img className="w-28 h-28 rounded-full" src={github} />
+            </div>
+            <h2 className="text-xl font-semibold mb-2">Check out my GitHub</h2>
+            <div className="flex justify-center">
+              <a
+                href="https://github.com/badri119"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded inline-block hover:scale-105 duration-500"
+              >
+                Visit Profile
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,64 +1,79 @@
-import React from 'react'
-import project1 from '../assets/project1.png'
-import project2 from '../assets/project2.png'
-import project3 from '../assets/project3.png'
+import React from "react";
+import project1 from "../assets/project1.png";
+import project2 from "../assets/project2.png";
+import project3 from "../assets/project3.png";
+import project4 from "../assets/addrecipe.png";
+import project5 from "../assets/wedding.png";
 const Projects = () => {
   const projects = [
     {
       id: 1,
-      src: project3,
-      href:'https://github.com/badri119/Search-Engine',
-      Text:'Java Search',
+      src: project4,
+      href: "https://github.com/badri119/Add-Recipe-Frontend",
+      Text: "AddRecipe",
     },
     {
       id: 2,
-      src: project1,
-      href: 'https://github.com/badri119/UWinCare',
-      Text: 'UWinCare',
+      src: project5,
+      href: "https://github.com/badri119/Wedding-Website",
+      Text: "Wedding Website",
     },
     {
       id: 3,
-      src: project2,
-      href:'https://github.com/badri119/FoodHunter-Recommendation-Engine',
-      Text:'FoodHunter'
+      src: project3,
+      href: "https://github.com/badri119/Search-Engine",
+      Text: "Java Search",
     },
-    
-    
+    {
+      id: 4,
+      src: project1,
+      href: "https://github.com/badri119/UWinCare",
+      Text: "UWinCare",
+    },
+    {
+      id: 5,
+      src: project2,
+      href: "https://github.com/badri119/FoodHunter-Recommendation-Engine",
+      Text: "FoodHunter",
+    },
   ];
   return (
     <div
-    name="projects"
-    className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen"
-  >
-    <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
-      <div className="pb-8">
-        <p className="text-4xl font-bold inline border-b-4 border-gray-500">
-          Projects
-        </p>
-        <p className="py-6">Check out the stuff that I worked on:</p>
-      </div>
+      name="projects"
+      className="bg-gradient-to-b from-black to-gray-800 w-full text-white"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            Projects
+          </p>
+          <p className="py-6">Check out the stuff that I worked on:</p>
+        </div>
 
-      <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-        {projects.map(({ id, src, href,Text }) => (
-          <a href = {href} target="_blank" rel="noreferrer">
-          <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
-            <img
-              src={src}
-              alt=""
-              className="rounded-md duration-200 hover:scale-105"
-            />
-            <div className="flex items-center justify-center">
-              <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 font-bold hover:text-gray-600 ">
-                {Text}
-              </button>
-            </div>
-          </div>
-          </a>
-        ))}
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
+          {projects.map(({ id, src, href, Text }) => (
+            <a href={href} target="_blank" rel="noreferrer">
+              <div
+                key={id}
+                className="shadow-md shadow-gray-600 rounded-lg h-86"
+              >
+                <img
+                  src={src}
+                  alt=""
+                  className="rounded-md duration-200 hover:scale-105 h-60 w-full object-cover bg-white"
+                />
+                <div className="flex items-center justify-center">
+                  <button className="w-100 px-6 py-3 m-4 duration-200 hover:scale-105 font-bold hover:text-gray-600 ">
+                    {Text}
+                  </button>
+                </div>
+              </div>
+            </a>
+          ))}
+        </div>
       </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
