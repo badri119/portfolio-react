6d088a56a8a3a97cb8c566df88e3c7023678a2f5
badri119 <badrinathv98@gmail.com>
2023-12-07 12:42:03 -0800

    background changes

--- src/components/Cycling.jsx
+++ src/components/Cycling.jsx
@@ -3,10 +3,7 @@ import Bike1 from "../assets/bike1.png";
 
 const Cycling = () => {
   return (
-    <div
-      name="cycling"
-      className=" w-full bg-gradient-to-b from-black to-gray-800 text-white  "
-    >
+    <div name="cycling">
       <div className=" max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className=" pb-8">
           <p className=" text-4xl font-bold inline border-b-4 border-gray-500 ">
